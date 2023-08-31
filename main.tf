3a6ba93954b2327d2b056c8dfc468c2a701a8f14
Aditya <aditya.mahule@cognizant.com>
2023-08-31 10:34:38 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+ resource "aws_s3_bucket_public_access_block" "access_good_1" {
+  bucket = aws_s3_bucket.bucket_good_1.id
+
+  block_public_acls   = true
+  block_public_policy = true
+
+ tags = {
+    Name = "foo-${data.aws_caller_identity.current.account_id}"
+  }
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+    target_prefix = "log/"
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.mykey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  
+   replication_configuration {
+    role = aws_iam_role.replication.arn
+    rules {
+      id     = "foobar"
+      prefix = "foo"
+      status = "Enabled"
+
+      destination {
+        bucket        = aws_s3_bucket.destination.arn
+        storage_class = "STANDARD"
+      }
+    }
+  }
+  
+  acl           = "private"
+  
+  
+}
